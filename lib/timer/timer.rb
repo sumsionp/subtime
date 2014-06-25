c9ab32684b5876922a3501e999d5c6764efba2cb
Peter Sumsion <sumsionp@gmail.com>
2014-06-25 06:57:07 -0600

    Add initial library and ruby-version files

---
+++ lib/timer/timer.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+class Timer
+# Takes the number of minutes, and a hash of messages to be said
+  def self.start(minutes, messages=nil)
+    # Alert user how many minutes the timer will run
+    puts "Starting timer for #{minutes} minutes..."
+  
+    # Set a loop to run through the number of minutes sleeping 60 sec for each one
+    minutes.downto 1 do |i|
+      sleep 60
+      puts i
+      if messages && messages[i]
+        `say #{messages[i]}`
+      end
+    end
+
+    # Play a chime when finished
+    `say timer done`
+  end
+end
