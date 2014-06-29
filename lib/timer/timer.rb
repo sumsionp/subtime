2c0c99b2cf628d036da9a542021298d711ccfc20
Peter Sumsion <sumsionp@gmail.com>
2014-06-28 22:26:15 -0600

    Use Timer class instance
    Variables that were passed between class methods
    fit better as instance variables.

--- lib/timer/timer.rb
+++ lib/timer/timer.rb
@@ -1,28 +1,32 @@
 #!/usr/bin/env ruby
 
 class Timer
-# Takes the number of minutes, and a hash of messages to be said
-  def self.start(minutes, messages=nil)
-    alert_start(minutes)
-    count_down(minutes, messages)
+  def initialize(minutes, messages=nil)
+    @minutes = minutes
+    @messages = messages
+  end
+
+  def start
+    alert_start
+    count_down
     play_done_chime
   end
 
-  def self.count_down(minutes, messages)
-    minutes.downto 1 do |i|
+  def count_down
+    @minutes.downto 1 do |i|
       sleep 60
       puts i
-      if messages && messages[i]
-        `say #{messages[i]}`
+      if @messages && @messages[i]
+        `say #{@messages[i]}`
       end
     end
   end
 
-  def self.alert_start(minutes)
-    puts "Starting timer for #{minutes} minutes..."
+  def alert_start
+    puts "Starting timer for #{@minutes} minutes..."
   end
 
-  def self.play_done_chime
+  def play_done_chime
     `say timer done`
   end
 end
