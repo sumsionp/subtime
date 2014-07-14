a47b679c0b990a36976ce3bb1404a178cf737d0c
Peter Sumsion <sumsionp@gmail.com>
2014-07-14 07:33:39 -0600

    Add tests for multiple minutes

--- spec/subtime/timer_spec.rb
+++ spec/subtime/timer_spec.rb
@@ -1,6 +1,11 @@
 require 'rspec/given'
 require 'subtime/timer'
 
+module Kernel
+  def sleep(seconds)
+  end
+end
+
 describe Timer do
 
   let(:output) { double('output').as_null_object }
@@ -20,6 +25,42 @@ describe Timer do
         expect(timer).to receive(:`).with("say timer done")
 
         timer.start
+      end
+    end
+
+    context "for 10 minutes" do
+      let(:minutes) { 10 }
+      let(:messages) { { 5 => "something", 2 => "something else" } }
+      let(:timer) { Timer.new(output, minutes, messages) }
+
+      it "outputs 'Starting timer for 10 minutes...'" do
+        expect(output).to receive(:puts).with("Starting timer for #{minutes} minutes...")
+
+        timer.start
+      end
+
+      it "outputs each minute from 10 down to 1" do
+        10.downto(1) do |minute|
+          expect(output).to receive(:puts).with(minute)
+        end
+
+        timer.start
+      end
+
+      context "with message 'something' at 5" do
+        it "says 'something'" do
+          expect(output).to receive(:`).with("say 'something'")
+
+          timer.start
+        end
+      end
+
+      context "with message 'something else' at 2" do
+        it "says 'something else'" do
+          expect(output).to receive(:`).with("say something else")
+
+          timer.start
+        end
       end
     end
   end
