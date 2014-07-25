a8f67753827aeff0cf4b4c626cc340c0de6ba74a
Peter Sumsion <sumsionp@gmail.com>
2014-07-25 07:06:58 -0600

    Remove conflict with RSpec::Matchers output

--- spec/subtime/timer_spec.rb
+++ spec/subtime/timer_spec.rb
@@ -8,15 +8,15 @@ end
 
 describe Timer do
 
-  let(:output) { double('output').as_null_object }
+  let(:timer_output) { double('timer_output').as_null_object }
 
   describe "#start" do
     context "without messages" do
       let(:minutes) { 0 }
-      let(:timer) { Timer.new(output, minutes) }
+      let(:timer) { Timer.new(timer_output, minutes) }
 
       it "outputs 'Starting timer for 0 minutes...'" do
-        expect(output).to receive(:puts).with("Starting timer for #{minutes} minutes...")
+        expect(timer_output).to receive(:puts).with("Starting timer for #{minutes} minutes...")
 
         timer.start
       end
@@ -26,25 +26,25 @@ describe Timer do
       let(:minutes) { 10 }
       let(:messages) { { 5 => lambda { puts "Something" },
                          2 => lambda { puts "something else" } } }
-      let(:timer) { Timer.new(output, minutes, messages) }
+      let(:timer) { Timer.new(timer_output, minutes, messages) }
 
       it "outputs 'Starting timer for 10 minutes...'" do
-        expect(output).to receive(:puts).with("Starting timer for #{minutes} minutes...")
+        expect(timer_output).to receive(:puts).with("Starting timer for #{minutes} minutes...")
 
         timer.start
       end
 
       it "outputs each minute from 10 down to 1" do
         10.downto(1) do |minute|
-          expect(output).to receive(:puts).with(minute)
+          expect(timer_output).to receive(:puts).with(minute)
         end
 
         timer.start
       end
 
       it "calls the specified code blocks" do
-        expect { timer.start }.to output.to_stdout
-        expect { timer.start }.to output('something else').to_stdout
+        expect { timer.start }.to output(/Something/).to_stdout
+        expect { timer.start }.to output(/something else/).to_stdout
       end
     end
   end
