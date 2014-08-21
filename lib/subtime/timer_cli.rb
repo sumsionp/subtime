c199c15b2c8427010313c5e7aeadbaf573e7ec02
Peter Sumsion <sumsionp@gmail.com>
2014-08-21 07:19:13 -0600

    Make timer script run with correct args
    Accept -m as an Integer for MINUTES
    Add -h to display help menu for Usage

--- lib/subtime/timer_cli.rb
+++ lib/subtime/timer_cli.rb
@@ -9,11 +9,23 @@ class TimerCLI
       opts.banner = "Usage: subtime [options]"
 
       opts.separator ""
+      opts.separator "Specific options:"
 
-      opts.on("-m", "--minutes MINUTES",
+      opts.on("-m", "--minutes MINUTES", Integer,
               "Total number of minutes to run timer") do |mins|
         options.minutes = mins
       end
+
+      opts.separator ""
+      opts.separator "Common options:"
+
+      opts.on_tail("-h", "--help", "Show this message") do
+        puts opts
+        exit
+      end
     end
+
+    opt_parser.parse!(args)
+    options
   end
 end
